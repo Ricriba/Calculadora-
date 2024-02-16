36917b17725e86e0afebe309e5dea350a045b9df
Richard Ferreira <richard.estelai@gmail.com>
2024-02-16 12:07:07 +0000

    commit inicial

---
+++ main.js
@@ -0,0 +1,13 @@
+//calculadora programa
+
+const display = document.getElementById("display");
+
+function appendTodisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {}
